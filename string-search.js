e1197bb3090942e0cc3576f234d6b915330955a1
tmescic <tihomir.mescic@gmail.com>
2014-11-11 01:27:08 +0100

    Implemented Boyer-Moore-Harspool

--- string-search.js
+++ string-search.js
@@ -18,15 +18,51 @@ function naive(needle, haystack) {
 	return -1;
 }
 
+
+
 /**
  * Boyer-Moore-Horspool algorithm
  */ 
-function boyer_moore_horspool (heedle, haystack) {
+function boyer_moore_horspool (needle, haystack) {
 
 	
 	var m = needle.length;
 	var n = haystack.length;
+	
+	// 128 ASCII characters, initialy fill with needle length
+	var skipTable = [];
+	for (var i=0; i < 128; i++) {
+		skipTable[i] = m;
+	}
+
+	for (var i=0; i < m-1; i++) {
+		skipTable[needle[i].charCodeAt(0)] = m - i - 1;
+	}
+
+	var i = 0;
+	while (i < n-m+1) {
+
+		var j = m-1;
+		for (; j >= 0 && needle[j] === haystack[i + j]; j--) {
+			
+		}
+
+		if (j === -1) { // bingo
+			return i;
+		}
+		i += skipTable[haystack[i + m -1].charCodeAt(0)];
+			
+	}
 
 	return -1;
 }
+
+
+
+var n = "C"
+var h = "AAAAAAAAAAACAABCCCCC";
+
+console.log("n : " + n + "\nh : " + h + "\nsolution(naive) : " + naive(n, h));
+console.log("solution(boyer-moore-horspool) : " + boyer_moore_horspool(n, h));
+
 
