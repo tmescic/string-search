2e34df1c38ff3ebbe4536420c7a25a6c3fc36b64
tmescic <tihomir.mescic@gmail.com>
2014-11-19 18:51:51 +0100

    Implemented Rabin-Karp text search

--- string-search.js
+++ string-search.js
@@ -57,12 +57,65 @@ function boyer_moore_horspool (needle, haystack) {
 	return -1;
 }
 
+/*
+ * Rabin-Karp search algorithm (uses a rolling hash).
+ */
+function rabin_karp(needle, haystack) {
+
+	var PRIME = 101;
+	
+
+	function hash(s) {
+
+		var h = 0.0;
+		for (var i = 0; i < s.length; i++) {
+			var c = s[i].charCodeAt(0);
+			h += c * Math.pow(PRIME, s.length - i - 1);
+		}
+
+		return h;
+	}
+
+	function nextHash(oldHash, oldChar, newChar, length) {
+
+		var next = oldHash - oldChar.charCodeAt(0) * Math.pow(PRIME, length-1);
+		next *= PRIME;
+		next += newChar.charCodeAt(0);
+
+		return next;
+
+	}
+
+	var needleHash = hash(needle);
+	var rollingHash = hash(haystack.substring(0, needle.length))
+
+	for (var i = 0; i <= haystack.length - needle.length; i++) {
+
+		// same hashes ?
+		if (needleHash === rollingHash) {
+
+			// check if the strings are the same
+			j = 0;
+			while (j < needle.length && needle[j] === haystack[i + j]) {
+		       j++;
+			}
+			if (j === needle.length) return i; // BINGO!!!
+		}
+
+		// get next hash
+		rollingHash = nextHash(rollingHash, haystack[i], haystack[i + needle.length], needle.length);
+	}
+
+	return -1;
+
+}
 
 
-var n = "C"
+var n = "D"
 var h = "AAAAAAAAAAACAABCCCCC";
 
-console.log("n : " + n + "\nh : " + h + "\nsolution(naive) : " + naive(n, h));
-console.log("solution(boyer-moore-horspool) : " + boyer_moore_horspool(n, h));
-
+console.log("n : " + n + "\nh : " + h + "\n");
+console.log("solution (naive)                : " + naive(n, h));
+console.log("solution (Boyer-Moore-Horspool) : " + boyer_moore_horspool(n, h));
+console.log("solution (Rabin-Harp)           : " + rabin_karp(n, h));
 
